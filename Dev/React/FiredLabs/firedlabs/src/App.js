425b71f7d8d94ad4bd5e105fea8bf99db19a5bba
Andre Passos <andrepassos.alp@gmail.com>
2020-11-24 18:28:53 -0300

    add dev

---
+++ Dev/React/FiredLabs/firedlabs/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './assets/img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <img>src = {logo} </img>
+  );
+}
+
+export default App;
---
+++ Dev/React/passosflix/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './components/Menu'
+import Carousel from './components/Carousel';
+
+function App() {
+  return (
+    <div> 
+     <Menu />
+     <Carousel />
+    </div>
+  );
+}
+
+export default App;
